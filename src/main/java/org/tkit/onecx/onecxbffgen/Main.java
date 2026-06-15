f984643d7c68a2a6334cd3efada61a4997bcb7ea
Maciej Kryger <50527026+maciejkryger@users.noreply.github.com>
2026-06-15 16:15:37 +0200

    feat: bff generator improvements

--- src/main/java/org/tkit/onecx/onecxbffgen/Main.java
+++ src/main/java/org/tkit/onecx/onecxbffgen/Main.java
@@ -1,31 +1,16 @@
 package org.tkit.onecx.onecxbffgen;
 
+import io.quarkus.picocli.runtime.annotations.TopCommand;
 import org.tkit.onecx.onecxbffgen.commands.CreateBffCommand;
-import org.tkit.onecx.onecxbffgen.service.GeneratorService;
 import picocli.CommandLine;
 
-import java.util.concurrent.Callable;
-
+@CommandLine.Command(
+        name = "onecx-bff-generator",
+        mixinStandardHelpOptions = true,
+        description = "OneCX BFF generator CLI",
+        subcommands = { CommandLine.HelpCommand.class, CreateBffCommand.class }
+)
+@TopCommand
 public class Main {
 
-    @CommandLine.Command(
-            name = "onecx-bff-generator",
-            mixinStandardHelpOptions = true,
-            description = "OneCX BFF generator CLI",
-            subcommands = { CommandLine.HelpCommand.class }
-    )
-    static class RootCommand implements Callable<Integer> {
-        @Override
-        public Integer call() {
-            CommandLine.usage(this, System.out);
-            return 0;
-        }
-    }
-
-    public static void main(String[] args) {
-        CommandLine cli = new CommandLine(new RootCommand());
-        cli.addSubcommand("create-bff", new CreateBffCommand(new GeneratorService()));
-        int exitCode = cli.execute(args);
-        System.exit(exitCode);
-    }
 }
